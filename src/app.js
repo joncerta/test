59943938f0050e7039cd00e1c4d2a83117a52de7
MacBook Air <jonathan.certain@apriorins.com>
2018-08-02 11:44:51 -0500

    add images

--- src/app.js
+++ src/app.js
@@ -10,40 +10,60 @@
             })
         };
 
-        model.getCharacters = function getCharacters(nameCharacter){
-                apiMarvelService.getCharacters(nameCharacter, function(response){
-                    console.log(response);
-                    if(response){
-                        if(response.status){
-                            model.characters = response.data.data.results;
-                            console.log(model.characters);
-                        }else{
-                                $log.error("Status is false:", response);
-                                model.characters = {};
-                        }
+        model.getAllCharacters = function getAllCharacters(){
+            limit = 100;
+            apiMarvelService.getAllCharacters(limit, function(response){
+                console.log(response);
+                if(response){
+                    if(response.status){
+                        model.characters = response.data.data.results;
+                        console.log(model.characters);
                     }else{
-                        $log.error('Server error');
-                        model.characters = {};
+                            $log.error("Status is false:", response);
+                            model.characters = {};
                     }
-                });
+                }else{
+                    $log.error('Server error');
+                    model.characters = {};
+                }
+            });
         };
 
+        model.getCharacters = function getCharacters(nameCharacter){
+            apiMarvelService.getCharacters(nameCharacter, function(response){
+                console.log(response);
+                if(response){
+                    if(response.status){
+                        model.characters = response.data.data.results;
+                        console.log(model.characters);
+                    }else{
+                            $log.error("Status is false:", response);
+                            model.characters = {};
+                    }
+                }else{
+                    $log.error('Server error');
+                    model.characters = {};
+                }
+            });
+        };
+
+        model.getAllCharacters();
 
     }]);
 
     app.service('connectApiMarvel', ['$http', function($http){
         var model = this;
 
-        model.endpoint = 'https://gateway.marvel.com:443/v1/public/characters';
+        model.endpoint = 'https://gateway.marvel.com:443';
         model.publicKey = "&apikey=9680732f936f2212243b63d0f868a98a";
         model.ts = 'ts=1';
         model.hash = "&hash=0cafc1b0a65703934c86a961f3cf2ad1";
 
-        model.getComcis = function (idCharacter) {
+        model.getAllCharacters = function (limit) {
             return $http({
                 method: 'GET',
                 data: {},
-                url: model.endpoint + '/' + idCharacter + '/comics?format=comic&' + model.ts + model.publicKey + model.hash
+                url: model.endpoint + '/v1/public/characters?' + 'limit='  + limit + '&' + model.ts + model.publicKey + model.hash
             });
         };
 
@@ -51,7 +71,7 @@
             return $http({
                 method: 'GET',
                 data: {},
-                url: model.endpoint + '?nameStartsWith=' + nameCharacter + '&' + model.ts + model.publicKey + model.hash
+                url: model.endpoint + '/v1/public/characters' + '?nameStartsWith=' + nameCharacter + '&' + model.ts + model.publicKey + model.hash
             });
         };
     }]);
@@ -62,6 +82,27 @@
         model.getComcis = function (idCharacter, callback) {
             var getComcisPromise = connectApiMarvel.getComcis(idCharacter, callback);
             getComcisPromise.then(
+                function success(response) {
+                    callback({
+                        status: response.status,
+                        data: response.data,
+                        info: response.info,
+                    });
+                },
+
+                function error(response) {
+                    callback({
+                        status: response.data.status,
+                        msg: response.data.error,
+                        error: response.data.error.code,
+                    });
+                }
+            );
+        };
+
+        model.getAllCharacters = function (limit, callback) {
+            var getAllCharactersPromise = connectApiMarvel.getAllCharacters(limit, callback);
+            getAllCharactersPromise.then(
                 function success(response) {
                     callback({
                         status: response.status,
